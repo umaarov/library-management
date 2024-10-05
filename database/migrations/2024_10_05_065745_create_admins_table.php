1d1a513ceaf5ba259aa2686c2ecbbe8d916ecdb4
Umarov Ismoiljon <hs.umarov21@gmail.com>
2024-10-05 12:07:06 +0500

    admins migration

--- database/migrations/2024_10_05_065745_create_admins_table.php
+++ database/migrations/2024_10_05_065745_create_admins_table.php
@@ -6,21 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up():
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
+            $table->string('email', 150)->unique();
+            $table->string('password');
+            $table->string('phone', 20);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down():
     {
         Schema::dropIfExists('admins');
     }
