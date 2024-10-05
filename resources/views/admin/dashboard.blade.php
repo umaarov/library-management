22fbff538bcf32f604dae435f19f9322e407c411
Umarov Ismoiljon <hs.umarov21@gmail.com>
2024-10-05 12:25:17 +0500

    Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,11 +1,33 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: hsuma
- * Date: 10/5/2024
- * Time: 11:51 AM
- */
+@extends('admin.layouts.app')
 
-?>
-
-Hi
+@section('content')
+    <div class="container">
+        <h1>Admin Dashboard</h1>
+        <div class="row">
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>{{ $totalBooks }}</h3>
+                        <p>Books</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>{{ $totalUsers }}</h3>
+                        <p>Users</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>{{ $totalLoans }}</h3>
+                        <p>Loans</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
