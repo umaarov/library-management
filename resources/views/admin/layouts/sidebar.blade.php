775ecb09e2841a8d3cddd652ebc26de79082dd61
Umarov Ismoiljon <hs.umarov21@gmail.com>
2024-10-08 18:10:29 +0500

    getting current user's email in sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -11,7 +11,7 @@
                 <img src="../../../template/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block">{{ Auth::user()->email }}</a>
             </div>
         </div>
 
